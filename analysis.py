71397384bf7c4951a1bed4277813d804c49861ea
riveruitm <riveruitm@gmail.com>
2026-01-12 20:42:10 +0100

    Jan Total Attempt

--- analysis.py
+++ analysis.py
@@ -7,3 +7,11 @@ with open('ucaccmet2j_python/precipitation.json') as file:
             if measurement["station"]=="GHCND:US1WAKG0038":
                 seattle.append(measurement)
 print(seattle)
+
+jan_total=0
+total_monthly_precipitation=[]
+for measurement in seattle:
+    if measurement["date"].startswith("2010-01"):
+         jan_total += measurement["value"]
+total_monthly_precipitation.insert(1, jan_total)
+print(total_monthly_precipitation)
