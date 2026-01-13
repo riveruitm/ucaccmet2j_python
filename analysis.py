ac8942a5ddfac627ec1a46ca30631cf34949b6a9
riveruitm <riveruitm@gmail.com>
2026-01-13 12:01:50 +0100

    Monthly + Yearly for all but manual

--- analysis.py
+++ analysis.py
@@ -2,22 +2,38 @@ import json
 import pandas as pd
 
 seattle=[]
+cincinnati=[]
+maui=[]
+sandiego=[]
 with open('ucaccmet2j_python/precipitation.json') as file:
     content=json.load(file)
     for measurement in content:
+        cleaner_date=measurement["date"].split("-")
+        cleaner_date.pop(0)
+        cleaner_date.pop(-1)
+        measurement["date"]=cleaner_date
+        for value in measurement["date"]:
+            value=int(value)
+            measurement["date"]=value
             if measurement["station"]=="GHCND:US1WAKG0038":
                 seattle.append(measurement)
+            if measurement["station"]=="GHCND:USW00093814":
+                cincinnati.append(measurement)
+            if measurement["station"]=="GHCND:USC00513317":
+                maui.append(measurement)
+            if measurement["station"]=="GHCND:US1WAKG0038":
+                sandiego.append(measurement)
 # print(seattle)
 
 months=set()
-for measurement in seattle:
-    cleaner_date=measurement["date"].split("-")
-    cleaner_date.pop(0)
-    cleaner_date.pop(-1)
-    measurement["date"]=cleaner_date
-    for value in measurement["date"]:
-        value=int(value)
-        measurement["date"]=value
+
+total_monthly_precipitation_cincinnati={}
+for measurement in cincinnati:
+    month=measurement["date"]
+    if month not in total_monthly_precipitation_cincinnati:
+        total_monthly_precipitation_cincinnati[month]=0
+    total_monthly_precipitation_cincinnati[month] += measurement["value"]
+print(f"Cincinnati: {total_monthly_precipitation_cincinnati}")
 
 total_monthly_precipitation_seattle={}
 for measurement in seattle:
@@ -25,11 +41,38 @@ for measurement in seattle:
     if month not in total_monthly_precipitation_seattle:
         total_monthly_precipitation_seattle[month]=0
     total_monthly_precipitation_seattle[month] += measurement["value"]
-print(total_monthly_precipitation_seattle)
+print(f"Seattle: {total_monthly_precipitation_seattle}")
+
+total_monthly_precipitation_maui={}
+for measurement in maui:
+    month=measurement["date"]
+    if month not in total_monthly_precipitation_maui:
+        total_monthly_precipitation_maui[month]=0
+    total_monthly_precipitation_maui[month] += measurement["value"]
+print(f"Maui: {total_monthly_precipitation_maui}")
+
+total_monthly_precipitation_sandiego={}
+for measurement in sandiego:
+    month=measurement["date"]
+    if month not in total_monthly_precipitation_sandiego:
+        total_monthly_precipitation_sandiego[month]=0
+    total_monthly_precipitation_sandiego[month] += measurement["value"]
+print(f"San Diego: {total_monthly_precipitation_sandiego}")
+
+total_monthly_precipitation_all=[]
+total_monthly_precipitation_all.append("Cincinnati:")
+total_monthly_precipitation_all.append(total_monthly_precipitation_cincinnati)
+total_monthly_precipitation_all.append("Seattle:")
+total_monthly_precipitation_all.append(total_monthly_precipitation_seattle)
+total_monthly_precipitation_all.append("Maui:")
+total_monthly_precipitation_all.append(total_monthly_precipitation_maui)
+total_monthly_precipitation_all.append("San Diego:")
+total_monthly_precipitation_all.append(total_monthly_precipitation_sandiego)
+
 
 # into JSON
 with open('ucaccmet2j_python/results.json', 'w', encoding='utf-8') as file:
-    json.dump(total_monthly_precipitation_seattle, file, indent=4, ensure_ascii=False)
+    json.dump(total_monthly_precipitation_all, file, indent=4, ensure_ascii=False)
 
 # Total yearly for locations:
 yr_all={}
@@ -50,13 +93,17 @@ for measurement in content:
     elif measurement["station"]=="GHCND:US1CASD0032":
             yr_sandiego += measurement["value"] 
             yr_all["San Diego"]=yr_sandiego
-print(yr_all)
 
-relative_monthly_precipitation_seattle={}
-for month in total_monthly_precipitation_seattle:
-    if month not in relative_monthly_precipitation_seattle:
-        relative_monthly_precipitation_seattle[month]=0
-    relative_monthly_precipitation_seattle[month] += total_monthly_precipitation_seattle[month]/yr_seattle
-print(relative_monthly_precipitation_seattle)
-with open('ucaccmet2j_python/results.json', 'a', encoding='utf-8') as file:
-    json.dump(relative_monthly_precipitation_seattle, file, indent=4, ensure_ascii=False)
+
+# relative_monthly_precipitation_area={}
+# for month in total_monthly_precipitation_area:
+#     if month not in relative_monthly_precipitation_area:
+#         relative_monthly_precipitation_area[month]=0
+#     relative_monthly_precipitation_area[month] += total_monthly_precipitation_seattle[month]/yr_seattle
+# print(relative_monthly_precipitation_area)
+
+# relative_monthly_precipitation_all=[]
+# relative_monthly_precipitation_all.append(relative_monthly_precipitation_area)
+
+# with open('ucaccmet2j_python/results.json', 'a', encoding='utf-8') as file:
+#     json.dump(relative_monthly_precipitation_all, file, indent=4, ensure_ascii=False)
